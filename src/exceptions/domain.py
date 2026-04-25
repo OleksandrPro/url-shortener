796249eda0c5fd6c4c8dffcf860d9c0ffad28246
Oleksandr <oleksandr.proshkin1@gmail.com>
2026-04-25 16:54:03 +0200

    feat: Check link expiration before redirect

--- src/exceptions/domain.py
+++ src/exceptions/domain.py
@@ -7,3 +7,7 @@ class UrlAlreadyExistsException(UrlShortenerError):
 class ShortUrlNotFoundError(UrlShortenerError):
     def __init__(self, url: str):
         super().__init__(f"Short url '{url}' doesn't exist")
+
+class ShortUrlExpired(UrlShortenerError):
+    def __init__(self, url: str):
+        super().__init__(f"Short url '{url}' has expired")
