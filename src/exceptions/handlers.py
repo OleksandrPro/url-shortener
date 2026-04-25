3c54aac71721358ab4ffae593500da48e3d30dac
Oleksandr <oleksandr.proshkin1@gmail.com>
2026-04-25 17:29:18 +0200

    feat: Return html-pages when link has expired or not found

--- src/exceptions/handlers.py
+++ src/exceptions/handlers.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI, Request, status
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, HTMLResponse
 
 from .base import UrlShortenerError
-from .domain import UrlAlreadyExistsException, ShortUrlNotFoundError
+from .domain import UrlAlreadyExistsException, ShortUrlNotFoundError, ShortUrlExpired
 
 EXCEPTION_STATUS_MAP = {
     UrlAlreadyExistsException: status.HTTP_400_BAD_REQUEST,
@@ -10,13 +10,43 @@ EXCEPTION_STATUS_MAP = {
     ShortUrlNotFoundError: status.HTTP_404_NOT_FOUND
 }
 
+NOT_FOUND = """
+<html>
+    <body>
+        <h1>Not found</h1>
+    </body>
+</html>
+"""
+
+URL_EXPIRED = """
+<html>
+    <body>
+        <h1>Url has expired!</h1>
+    </body>
+</html>
+"""
+
 def register_exception_handlers(app: FastAPI):
     @app.exception_handler(UrlShortenerError)
     async def domain_exception_handler(request: Request, exc: UrlShortenerError):
 
         status_code = EXCEPTION_STATUS_MAP.get(type(exc), status.HTTP_400_BAD_REQUEST)
 
-        return JSONResponse(
-            status_code=status_code,
-            content={"detail": str(exc)},
-        )
+        match exc:
+            case UrlAlreadyExistsException():
+                response = JSONResponse(
+                    status_code=status_code,
+                    content={"detail": str(exc)},
+                )
+            case ShortUrlNotFoundError():
+                response = HTMLResponse(
+                    status_code=status_code,
+                    content=NOT_FOUND
+                )
+            case ShortUrlExpired():
+                response = HTMLResponse(
+                    status_code=status_code,
+                    content=URL_EXPIRED
+                )
+
+        return response
