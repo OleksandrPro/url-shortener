00976043625238341d9b0f2c7350ffd0802af9a9
Oleksandr <oleksandr.proshkin1@gmail.com>
2026-04-26 09:55:36 +0200

    chore: Delete commented code

--- src/services/short_url_generator.py
+++ src/services/short_url_generator.py
@@ -3,15 +3,7 @@ from src.utils.encoding.base62 import encode
 
 def generate_short_url(long_url: str, c_servise: CounterService):
     new_count = c_servise.get_next_count()
-    """
-    
-    val = 3900
-    short_url = encode(val)
-    init_long_url = decode(short_url)
 
-    print(f"----short_url: {short_url}----")
-    print(f"----init_long_url: {init_long_url}----")
-    """
     try:
         short_url = encode(new_count)
         c_servise.increment_counter()
